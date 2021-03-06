093e80e66f34c84e7f89c06f50639885b144111d
Victor Cabrera Lopes Cardoso <victor.cabrera0209@gmail.com>
2021-03-06 07:24:14 -0300

    Resolução com correção/diminuição de grau de complexidade do Requisito 6

--- src/challenges.js
+++ src/challenges.js
@@ -34,12 +34,10 @@ function highestCount(arr) {
   let bigger = 0;
   let biggerrepeat = 0;
   for (let index = 0; index < arr.length; index += 1) {
-    if (arr[index] >= bigger) {
+    if (arr[index] > bigger) {
       bigger = arr[index];
-    }
-  }
-  for (let index = 0; index < arr.length; index += 1) {
-    if (arr[index] === bigger) {
+      biggerrepeat = 1;
+    } else if (arr[index] === bigger) {
       biggerrepeat += 1;
     }
   }
