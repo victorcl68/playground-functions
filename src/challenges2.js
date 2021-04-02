ea07b4476da1ee1aca034bbcace1c3cbec92ed31
Victor Cabrera Lopes Cardoso <victor.cabrera0209@gmail.com>
2021-04-02 01:51:19 -0300

    Resolução requisito 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,8 +1,22 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(theArray, theString) {
+  let output = [];
+  let lastArray = [];
+  const arrrayInOrder = theArray.sort();
+  if (theArray.length > 0) {
+    for (let index = 0; index < theArray.length; index += 1) {
+      let theObject = {
+        tech: arrrayInOrder[index],
+        name: theString,
+      };
+      lastArray.push(theObject);
+    }
+    output = lastArray;
+  } else {
+    output = 'Vazio!';
+  }
+  return output;
 }
-
 // Desafio 11
 function generatePhoneNumber() {
   // seu código aqui
